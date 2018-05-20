ac05c5d3ac91e2e1bc03e64a634106bca2a9d54c
Vova Pihol <sg.volodymyr@gmail.com>
2018-05-20 09:14:35 +0300

    Fix controls

--- public/game/builder.js
+++ public/game/builder.js
@@ -2,64 +2,6 @@ let models_count = 7, models_loaded = 0,
   pikalka, st, mt, kokoro, kokoro_roof, roof, gift, proof;
 
 function startGame(data){
-  var blocker = document.getElementById( 'blocker' );
-  var instructions = document.getElementById( 'instructions' );
-
-  var havePointerLock = 'pointerLockElement' in document || 'mozPointerLockElement' in document || 'webkitPointerLockElement' in document;
-
-  if ( havePointerLock ) {
-  var element = document.body;
-
-  var pointerlockchange = function ( event ) {
-
-    if(document.pointerLockElement === element || document.mozPointerLockElement === element || document.webkitPointerLockElement === element){
-      controlsEnabled = true;
-      controls.enabled = true;
-      blocker.style.display = 'none';
-    }else{
-      controls.enabled = false;
-      blocker.style.display = 'block';
-      instructions.style.display = '';
-    }
-  };
-
-  var pointerlockerror = function ( event ) {
-    instructions.style.display = '';
-  };
-
-  // Hook pointer lock state change events
-  document.addEventListener( 'pointerlockchange', pointerlockchange, false );
-  document.addEventListener( 'mozpointerlockchange', pointerlockchange, false );
-  document.addEventListener( 'webkitpointerlockchange', pointerlockchange, false );
-
-  document.addEventListener( 'pointerlockerror', pointerlockerror, false );
-  document.addEventListener( 'mozpointerlockerror', pointerlockerror, false );
-  document.addEventListener( 'webkitpointerlockerror', pointerlockerror, false );
-
-  instructions.addEventListener( 'click', function ( event ) {
-    instructions.style.display = 'none';
-
-    // Ask the browser to lock the pointer
-    element.requestPointerLock = element.requestPointerLock || element.mozRequestPointerLock || element.webkitRequestPointerLock;
-    element.requestPointerLock();
-  }, false );
-  }else{
-    instructions.innerHTML = 'Your browser doesn\'t seem to support Pointer Lock API';
-  }
-
-  var controlsEnabled = false;
-
-  var moveForward = false;
-  var moveBackward = false;
-  var moveLeft = false;
-  var moveRight = false;
-  var canJump = false;
-
-  var prevTime = performance.now();
-  var velocity = new THREE.Vector3();
-  var direction = new THREE.Vector3();
-  var vertex = new THREE.Vector3();
-  var color = new THREE.Color();
 
   initThreeJs();
   prepareScene();
